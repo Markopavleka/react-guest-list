86133df8fe3d13498e8bc57af9fa50f4d6985291
Marko Pavleka <markopavleka@gmx.at>
2023-09-25 17:59:59 +0200

    Fix http to https

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import './App.css';
 import { useEffect, useState } from 'react';
 
 const baseUrl =
-  'http://express-guest-list-api-memory-data-store-1--markopavleka.repl.co/';
+  'https://express-guest-list-api-memory-data-store-1--markopavleka.repl.co/';
 
 export default function App() {
   const [guestName, setGuestName] = useState('');
