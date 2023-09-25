9247c7db32b23fff170d88f03fc8d4cfa716eb16
Marko Pavleka <markopavleka@gmx.at>
2023-09-25 16:49:03 +0200

    Add api function

--- src/App.js
+++ src/App.js
@@ -1,46 +1,157 @@
 import './App.css';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 const baseUrl = 'http://localhost:4000';
-const response = await fetch(`${baseUrl}/guests`);
-const allGuests = await response.json();
 
 export default function App() {
   const [guestName, setGuestName] = useState('');
   const [guestLastName, setGuestLastName] = useState('');
   const [guest, setGuest] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
 
-  const handleGuestAttend = (index) => {
-    const updateAttending = [...guest];
-    updateAttending[index].attend = !updateAttending[index].attend;
-    setGuest(updateAttending);
-  };
-  const handleGuestName = (event) => {
-    if (event.key === 'Enter') {
-      const newGuestId = guest.length > 0 ? guest[guest.length - 1].id + 1 : 1;
+  useEffect(() => {
+    fetch(`${baseUrl}/guests`)
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+        return response.json();
+      })
+      .then((data) => {
+        setGuest(data);
+        setIsLoading(false);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }, [guest]);
+
+  if (isLoading) {
+    return 'Loading...';
+  }
+  const sendGuestData = async function (firstName, lastName) {
+    try {
       const newGuest = {
-        id: newGuestId,
-        name: {
-          firstName: guestName,
-          lastName: guestLastName,
-        },
-        attend: false,
+        firstName,
+        lastName,
+        attending: false,
       };
-      setGuest([...guest, newGuest]);
       console.log(newGuest);
+      const response = await fetch(`${baseUrl}/guests`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(newGuest),
+      });
+
+      const responseData = await response.json();
+      console.log('Response data:', responseData);
+
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
+      }
+
       setGuestName('');
       setGuestLastName('');
+    } catch (error) {
+      console.error('Error sending guest data:', error);
     }
   };
-  const handleDeleteUser = (indexToDelete) => {
-    const updatedUsers = [...guest];
-    updatedUsers.splice(indexToDelete, 1);
+  /* const deleteAllGuests = async (id) => {
+    try {
+      const response = await fetch(`${baseUrl}/guests/${id}`, {
+        method: 'DELETE',
+      });
 
-    setGuest(updatedUsers);
+      if (!response.ok) {
+        throw new Error('Failed to delete guest');
+      }
+
+      const updatedGuest = guest.map((guestById) => guestById.id === id);
+      setGuest(updatedGuest);
+    } catch (error) {
+      console.error('Error deleting guest:', error);
+    }
+  };*/
+
+  const deleteGuest = async (id) => {
+    try {
+      const response = await fetch(`${baseUrl}/guests/${id}`, {
+        method: 'DELETE',
+      });
+
+      if (!response.ok) {
+        throw new Error('Failed to delete guest');
+      }
+
+      const updatedGuest = guest.filter((guestById) => guestById.id !== id);
+      setGuest(updatedGuest);
+    } catch (error) {
+      console.error('Error deleting guest:', error);
+    }
+  };
+
+  const handleGuestAttendTrue = (id) => {
+    fetch(`${baseUrl}/guests/${id}`, {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ attending: true }),
+    })
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error('Failed to update guest attendance');
+        }
+        return response.json();
+      })
+      .then(() => {
+        const updatedGuests = guest.map((user) => {
+          if (user.id === id && user.attending === false) {
+            return { ...user, attending: true };
+          }
+          return user;
+        });
+        console.log(updatedGuests);
+        setGuest(updatedGuests);
+      })
+      .catch((error) => {
+        console.error('Error updating guest attendance:', error);
+      });
+  };
+  const handleGuestAttendFalse = (id) => {
+    fetch(`${baseUrl}/guests/${id}`, {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ attending: false }),
+    })
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error('Failed to update guest attendance');
+        }
+        return response.json();
+      })
+      .then(() => {
+        const updatedGuests = guest.map((user) => {
+          if (user.id === id && user.attending === true) {
+            return { ...user, attending: false };
+          }
+          return user;
+        });
+        console.log(updatedGuests);
+        setGuest(updatedGuests);
+      })
+      .catch((error) => {
+        console.error('Error updating guest attendance:', error);
+      });
   };
 
   return (
     <div className="Container">
+      <h1>Guest List</h1>
       <label htmlFor="firstName" data-test-id="guest">
         <input
           data-test-id="guest"
@@ -62,21 +173,26 @@ export default function App() {
           onChange={(event) => {
             setGuestLastName(event.currentTarget.value);
           }}
-          onKeyDown={handleGuestName}
+          onKeyDown={async (event) => {
+            if (event.key === 'Enter') {
+              await sendGuestData(guestName, guestLastName);
+            }
+          }}
           required
         />
       </label>
+
       <br />
       <div className="GuestContainer">
-        {guest.map((user, index) => {
+        {guest.map((user) => {
           return (
             <div key={`user-id-${user.id} `}>
               <div data-test-id="guest">
-                {user.name.firstName} {user.name.lastName}
+                {user.firstName} {user.lastName}
                 <button
                   aria-label="Remove"
                   key={`user-id-${user.id} `}
-                  onClick={() => handleDeleteUser(index)}
+                  onClick={() => deleteGuest(user.id)}
                 >
                   X
                 </button>
@@ -85,12 +201,16 @@ export default function App() {
                   <input
                     type="checkbox"
                     aria-label="attending"
-                    checked={user.attend}
+                    checked={user.attending}
                     onChange={() => {
-                      handleGuestAttend(index);
+                      if (user.attending === false) {
+                        handleGuestAttendTrue(user.id);
+                      } else {
+                        handleGuestAttendFalse(user.id);
+                      }
                     }}
                   />
-                  {user.attend ? 'attending' : 'not attending'}
+                  {user.attending ? 'attending' : 'not attending'}
                 </div>
               </div>
             </div>
